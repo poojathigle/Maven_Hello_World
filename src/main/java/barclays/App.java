6994ebc21ea08fe8d084f4bda87be92955834ec9
Pooja Thigle <poojathigle.2016@gmail.com>
2022-04-06 16:15:24 +0530

    added files

---
+++ src/main/java/barclays/App.java
@@ -0,0 +1,13 @@
+package barclays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
